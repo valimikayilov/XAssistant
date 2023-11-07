0249a6c4ce38859dc82a83aa973d3d039b391deb
Vali <valimikayilov@yahoo.com>
2023-11-07 04:10:20 +0100

    upload

---
+++ XAssistant/XAssistant/main.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  XAssistant
+//
+//  Created by vali on 05.11.23.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+func shell(_ command: String) -> String {
+    let task = Process()
+    let pipe = Pipe()
+    
+    task.standardOutput = pipe
+    task.standardError = pipe
+    task.arguments = ["-c", command]
+    task.launchPath = "/bin/zsh"
+    task.standardInput = nil
+    task.launch()
+    
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: .utf8)!
+    
+    return output
+}
+
+shell("ls")
